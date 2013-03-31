fd44d747af9f51f3679ae2ffa61a3b503e8517e8
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-31 21:32:34 +0000

    version number
    git-svn-id: https://plugins.svn.wordpress.org/cookie-ok/trunk@689639 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cookieok.php
+++ cookieok.php
@@ -10,7 +10,7 @@ Author URI: http://digital-morphosis.com
 
 function cookieok_enqueue_scripts(){
 	wp_enqueue_script('jquery-cookie',plugins_url('/js/jquery.cookie.js', __FILE__), array('jquery'), '1.3', true);
-	wp_enqueue_script('cookieok',plugins_url('/js/cookieok.js', __FILE__), array('jquery'), '0.2', true);
+	wp_enqueue_script('cookieok',plugins_url('/js/cookieok.js', __FILE__), array('jquery'), '0.3', true);
 }
 add_action('wp_enqueue_scripts', 'cookieok_enqueue_scripts');
 
